285b4e794726927ea3a94754384b7371f4e7661a
Robert DeRouin <robert@crowdbotics.com>
2022-09-26 09:25:44 -0700

    Crowdbotics Studio: Update app screens

--- screens/about-the-app/index.js
+++ screens/about-the-app/index.js
@@ -3,13 +3,12 @@ import { Text, StyleSheet, View } from "react-native";
 
 const AboutTheAppScreen = params => {
   return <View style={styles.container}>
-      <Text style={styles.headingTxt}>Ideas to code in minutes.</Text>
-    </View>;
+      <Text style={styles.headingTxt}>Ideas to code.</Text>
+    <View style={styles.tJTmcqiF}></View></View>;
 };
 
 const styles = StyleSheet.create({
   container: {
-    margin: 20,
     flex: 1,
     justifyContent: "space-around",
     backgroundColor: "#12054a"
@@ -20,12 +19,24 @@ const styles = StyleSheet.create({
     padding: 2,
     marginVertical: 12,
     position: "absolute",
-    left: 4,
-    top: 301,
+    left: 26,
+    top: 118,
     textAlign: "center",
     width: 318,
     height: 36,
     color: "#fff"
+  },
+  tJTmcqiF: {
+    left: 62,
+    top: 131,
+    position: "absolute",
+    height: 40,
+    width: 241,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 10,
+    color: "#777777",
+    opacity: 0.2,
+    transform: "rotate(0deg)"
   }
 });
 export default AboutTheAppScreen;
