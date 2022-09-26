d0bf6eb10c62b0e8c3f034b46837fb66df1bdb44
Robert DeRouin <robert@crowdbotics.com>
2022-09-26 09:23:11 -0700

    Crowdbotics Studio: Update app screens

--- screens/about-the-app/index.js
+++ screens/about-the-app/index.js
@@ -11,7 +11,8 @@ const styles = StyleSheet.create({
   container: {
     margin: 20,
     flex: 1,
-    justifyContent: "space-around"
+    justifyContent: "space-around",
+    backgroundColor: "#12054a"
   },
   headingTxt: {
     fontSize: 24,
@@ -23,7 +24,8 @@ const styles = StyleSheet.create({
     top: 301,
     textAlign: "center",
     width: 318,
-    height: 36
+    height: 36,
+    color: "#fff"
   }
 });
 export default AboutTheAppScreen;
